10dcc0a0e6fa66f2054c3516c9d3183188b647d6
Gaurav Singh <gks858687@gmail.com>
2025-10-24 21:52:00 +0530

    analytics code added

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F996MEE4EH"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-F996MEE4EH');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased">
         <AuthProvider>
           <CartProvider>
