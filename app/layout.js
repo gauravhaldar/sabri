5d8fc4ccf6860a61d6d358ac011c0b93ba4a7ca7
Batman@0001 <santrasuraj14@gmail.com>
2025-10-27 22:34:04 +0530

    add the meta pixel code

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F996MEE4EH"></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-F996MEE4EH"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -30,6 +33,33 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1244423037727066');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1244423037727066&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className="antialiased">
         <AuthProvider>
